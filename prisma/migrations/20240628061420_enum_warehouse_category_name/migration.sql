82f1815227f7b8eaf14e47243f1e412f59a40fe9
ahmadafyouni91 <ahmad.aafyouni@gmail.com>
2024-07-29 21:42:04 +0200

    new Prisma

--- prisma/migrations/20240628061420_enum_warehouse_category_name/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - Changed the type of `CategoryName` on the `Warehouses` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
-
-*/
--- AlterTable
-ALTER TABLE "Warehouses" DROP COLUMN "CategoryName",
-ADD COLUMN     "CategoryName" "CategoryName" NOT NULL;
--- prisma/migrations/20240628130721_supplier_description/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `email` on the `Suppliers` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Suppliers" DROP COLUMN "email",
-ADD COLUMN     "Description" TEXT;
