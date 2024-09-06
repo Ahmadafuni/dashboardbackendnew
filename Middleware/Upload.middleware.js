2b4bc096b70d44d6dfef547f8822782c548e7ada
ahmadafyouni91 <ahmad.aafyouni@gmail.com>
2024-09-06 23:52:21 +0200

    File restart

--- Middleware/Upload.middleware.js
+++ Middleware/Upload.middleware.js
@@ -1,39 +1,72 @@
 import multer from "multer";
-import fs from "fs";
-import path from "path";
 
-// Utility function to ensure the directory exists or create it
-const ensureDirectoryExists = (dirPath) => {
-  if (!fs.existsSync(dirPath)) {
-    fs.mkdirSync(dirPath, { recursive: true }); // Create the directory if it doesn't exist
-  }
-};
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/templates");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(
+      null,
+      file.fieldname + "-" + uniqueSuffix + "." + file.mimetype.split("/")[1]
+    );
+  },
+});
 
-// Utility function to create multer storage configuration
-const createStorage = (destinationPath) => {
-  return multer.diskStorage({
-    destination: function (req, file, cb) {
-      ensureDirectoryExists(destinationPath); // Ensure the directory exists
-      cb(null, destinationPath);
-    },
-    filename: function (req, file, cb) {
-      const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-      const ext = path.extname(file.originalname); // Use the original file extension
-      cb(null, `${file.fieldname}-${uniqueSuffix}${ext}`);
-    },
-  });
-};
+const profileStorage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/profiles");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(
+      null,
+      file.fieldname + "-" + uniqueSuffix + "." + file.mimetype.split("/")[1]
+    );
+  },
+});
 
-// Define storage configurations for different file types
-const templateStorage = createStorage("public/templates");
-const profileStorage = createStorage("public/profiles");
-const modelStorage = createStorage("public/models");
-const orderStorage = createStorage("public/orders");
-const taskStorage = createStorage("public/tasks");
+const modelStorage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/models");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(
+      null,
+      file.fieldname + "-" + uniqueSuffix + "." + file.mimetype.split("/")[1]
+    );
+  },
+});
 
-// Multer upload configurations
-export const uploadTemplate = multer({ storage: templateStorage });
+const modelOrder = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/orders");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(
+      null,
+      file.fieldname + "-" + uniqueSuffix + "." + file.mimetype.split("/")[1]
+    );
+  },
+});
+
+const modelTask = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/tasks");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(
+      null,
+      file.fieldname + "-" + uniqueSuffix + "." + file.mimetype.split("/")[1]
+    );
+  },
+});
+
+export const upload = multer({ storage: storage });
 export const uploadProfile = multer({ storage: profileStorage });
 export const uploadModel = multer({ storage: modelStorage });
-export const uploadOrder = multer({ storage: orderStorage });
-export const uploadTask = multer({ storage: taskStorage });
+export const uploadOrder = multer({ storage: modelOrder });
+export const uploadTask = multer({ storage: modelTask });
