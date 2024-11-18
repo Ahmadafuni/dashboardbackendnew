00e6c5ab8a256a58babbfc7613cd3af0c01e3d30
AhmadAfyouni <ahmad.aafyouni@gmail.com>
2024-11-18 23:51:48 +0100

    change prisma

--- prisma/migrations/20240901194328_manufacturingstagemodel/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `IsArchived` on table `Collections` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE "Collections" ALTER COLUMN "IsArchived" SET NOT NULL;
